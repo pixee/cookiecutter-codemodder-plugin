47f5594914bd784dc82b9acf0ed0383ed9c2e4bd
Daniel D'Avella <dan.davella@pixee.ai>
2024-02-19 10:35:17 -0500

    Update codemodder dependency; remove unused fields

--- src/custom_codemods/__init__.py
+++ src/custom_codemods/__init__.py
@@ -5,8 +5,6 @@ from .custom_codemod import CustomCodemod
 # This name is used by the entry point in pyproject.toml to register the codemods
 registry = CodemodCollection(
     origin="custom", # TODO: Change this to your own project name
-    docs_module="custom_codemods.docs",
-    semgrep_config_module="custom_codemods.semgrep",
     codemods=[
         CustomCodemod,
     ],
